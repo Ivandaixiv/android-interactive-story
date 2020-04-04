e7576c2b8c270a24ce2be63b65ae6f0cf6d5fecf
Ivan <ivandaixivwork@gmail.com>
2020-04-03 22:36:42 -0700

    Adds toast for text input

---
+++ app/src/main/java/com/ivandai/interactivestory/StoryActivity.java
@@ -0,0 +1,14 @@
+package com.ivandai.interactivestory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story);
+    }
+}
