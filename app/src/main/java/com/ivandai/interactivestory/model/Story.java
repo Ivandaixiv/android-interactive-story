74db99246d5160226d7af543a9d650876100de1a
Ivan <ivandaixivwork@gmail.com>
2020-04-10 20:59:58 -0700

    Adds a dynamic image to the story page

--- app/src/main/java/com/ivandai/interactivestory/model/Story.java
+++ app/src/main/java/com/ivandai/interactivestory/model/Story.java
@@ -40,6 +40,9 @@ public class Story {
     }
 
     public Page getPage(int pageNumber) {
+        if(pageNumber >= pages.length) {
+            pageNumber = 0;
+        }
         return pages[pageNumber];
     }
 }
--- app/src/main/java/com/ivandai/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/com/ivandai/interactivestory/ui/StoryActivity.java
@@ -1,10 +1,15 @@
 package com.ivandai.interactivestory.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.ivandai.interactivestory.R;
 import com.ivandai.interactivestory.model.Page;
@@ -15,11 +20,19 @@ public class StoryActivity extends AppCompatActivity {
     public static final String TAG = StoryActivity.class.getSimpleName();
 
     private Story story;
+    private ImageView storyImageView;
+    private TextView storyTextView;
+    private Button choice1Button;
+    private Button choice2Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
+
+        storyImageView = findViewById(R.id.storyImageView);
+        choice1Button = findViewById(R.id.choice1Button);
+        choice2Button = findViewById(R.id.choice2Button);
 
         Intent intent = getIntent();
         String name = intent.getStringExtra(getString(R.string.key_name));
@@ -34,6 +47,8 @@ public class StoryActivity extends AppCompatActivity {
     }
 
     private void loadPage(int pageNumber) {
-        Page page = story.getPage(pageNumber)
+        Page page = story.getPage(pageNumber);
+        Drawable image = ContextCompat.getDrawable(this, page.getImageId());
+        storyImageView.setImageDrawable(image);
     }
 }
